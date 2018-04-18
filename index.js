5b38aa176ecf6fafcb8699fc3a242006a4efd80e
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-18 19:47:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function dec(n) {
   return n-= 1;
 }
 
-// function makeInt(n) {
-//   return parseInt(n, 10);
-// }
+function makeInt(n) {
+  return parseInt(n, 10);
+}
 
 function preserveDecimal(n) {
   return parseFloat(n);
